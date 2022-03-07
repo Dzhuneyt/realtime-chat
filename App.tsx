4f697bd68bb6d6a7b8a8c8cbccae76fe30d12a53
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2022-03-07 17:56:08 +0200

    feat: Firebase auth

--- App.tsx
+++ App.tsx
@@ -2,22 +2,70 @@
  * @format
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, StyleSheet, Text, useColorScheme} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import type {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
 
-const Stack = createNativeStackNavigator();
+type RootStackParamList = {
+  Home: undefined;
+  ChatRooms: undefined;
+};
 
-const HomeScreen = () => {
-  return <Text>Home Screen</Text>;
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
+
+const HomeScreen = (props: HomeScreenProps) => {
+  // Handle user state changes
+  const onAuthStateChanged: FirebaseAuthTypes.AuthListenerCallback = u => {
+    if (u !== null) {
+      props.navigation.replace('ChatRooms');
+    }
+  };
+
+  useEffect(() => {
+    return auth().onAuthStateChanged(onAuthStateChanged); // unsubscribe on unmount
+  });
+
+  useEffect(() => {
+    // Trigger anonymous login
+    auth()
+      .signInAnonymously()
+      .then(() => {
+        console.log('User signed in anonymously');
+      })
+      .catch(error => {
+        if (error.code === 'auth/operation-not-allowed') {
+          console.error('Enable anonymous in your firebase console.');
+        }
+
+        console.error(error);
+      });
+  }, []);
+
+  return <></>;
+};
+
+type ChatRoomsProps = NativeStackScreenProps<RootStackParamList, 'ChatRooms'>;
+const ChatRooms = (props: ChatRoomsProps) => {
+  const [user, setUser] = useState<FirebaseAuthTypes.User>();
+  useEffect(() => {
+    return auth().onAuthStateChanged(u => {
+      if (u === null) {
+        // Logged out. Redirect to login screen
+        props.navigation.replace('Home');
+        return;
+      }
+      if (u) {
+        setUser(u);
+      }
+    });
+  });
+  return <Text>Chat rooms for {user?.uid}</Text>;
 };
 
 const App = () => {
@@ -35,6 +83,11 @@ const App = () => {
             name="Home"
             component={HomeScreen}
             options={{title: 'Loading'}}
+          />
+          <Stack.Screen
+            name="ChatRooms"
+            component={ChatRooms}
+            options={{title: 'Chat Rooms'}}
           />
         </Stack.Navigator>
       </NavigationContainer>
